e2f72ed60e65abe14bab9491d4ddef359cfc4d2e
yohanes14 <ycrusc@ist.id>
2025-04-28 21:38:20 +0700

    add user

---
+++ app/Filament/Resources/UserManagementResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserManagementResource\Pages;
+use App\Filament\Resources\UserManagementResource\RelationManagers;
+use App\Models\User;
+use App\Models\UserManagement;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserManagementResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name'),
+                TextInput::make('email'),
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('email'),
+                TextColumn::make('role'),
+                TextColumn::make('status')
+                    ->label('Status')
+                    ->formatStateUsing(fn($state) => $state ? 'Active' : 'Not Active')
+                    ->sortable(),
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUserManagement::route('/'),
+            'create' => Pages\CreateUserManagement::route('/create'),
+            'edit' => Pages\EditUserManagement::route('/{record}/edit'),
+        ];
+    }
+}
