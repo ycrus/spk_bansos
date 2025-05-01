6ceb8a08b0980c5a2e028e389eda286365c838cf
yohanes14 <ycrusc@ist.id>
2025-05-01 21:08:18 +0700

    fa

--- public/index.php
+++ public/index.php
@@ -2,26 +2,26 @@
 
 use Illuminate\Http\Request;
 
-// define('LARAVEL_START', microtime(true));
+define('LARAVEL_START', microtime(true));
 
-// // Determine if the application is in maintenance mode...
-// if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
-//     require $maintenance;
-// }
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-// // Register the Composer autoloader...
-// require __DIR__ . '/../vendor/autoload.php';
+// Register the Composer autoloader...
+require __DIR__ . '/../vendor/autoload.php';
 
 // // Bootstrap Laravel and handle the request...
 // (require_once __DIR__ . '/../bootstrap/app.php')
 //     ->handleRequest(Request::capture());
 
-// $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-// $response = $kernel->handle(
-//     $request = Illuminate\Http\Request::capture()
-// );
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-// $response->send();
+$response->send();
 
-// $kernel->terminate($request, $response);
+$kernel->terminate($request, $response);
