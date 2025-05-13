1bbdebc809b6f1591dc29ce078ecab4a103a66af
ycrusc <ycrusc@gmail.com>
2025-05-13 17:00:21 +0700

    ds

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 Route::get('/', function () {
-    return redirect('/admin'); // Ganti '/admin' sesuai prefix panel Filament-mu
+    return redirect('/admin'); 
 });
